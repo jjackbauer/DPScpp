d710948a2737bd17be0f3f80c90774e843c12b13
jjackbauerx <j_r_style@hotmail.com>
2021-03-08 15:30:40 -0300

    ImpulseResponse class added

---
+++ ImpulseResponse.hpp
@@ -0,0 +1,15 @@
+/*
+ * ImpulseResponse.hpp
+ *
+ *  Created on: 8 de mar. de 2021
+ *      Author: ricardo
+ */
+
+#ifndef DPSCPP_IMPULSERESPONSE_HPP_
+#define DPSCPP_IMPULSERESPONSE_HPP_
+
+
+
+
+
+#endif /* DPSCPP_IMPULSERESPONSE_HPP_ */
---
+++ InpulseResponse.cpp
@@ -0,0 +1,8 @@
+/*
+ * InpulseResponse.cpp
+ *
+ *  Created on: 8 de mar. de 2021
+ *      Author: ricardo
+ */
+#include "ImpulseResponse.hpp"
+
