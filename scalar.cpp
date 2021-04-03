e5af56507353021dabf767f70f22a41f45577a4b
jjackbauer <j_r_style@hotmail.com>
2021-04-02 21:42:41 -0300

    scaling operation implemented

--- scalar.cpp
+++ scalar.cpp
@@ -1,4 +1,53 @@
 #include "scalar.hpp"
+scalar::scalar(std::complex<double> k,signal *x)
+{
+	setK(k);
+	setX(x);
+}
+scalar::scalar(scalar &toCopy) noexcept : k{toCopy.getK()}, x{toCopy.getX()}
+{
 
+}
+scalar& scalar::operator=(scalar &toCopy) noexcept
+{
+	setK(toCopy.getK());
+	setX(toCopy.getX());
+	return *this;
+}
+scalar::scalar(scalar &&toMove) noexcept :k{std::move(toMove.getK())}, x{std::move(toMove.getX())}
+{
 
+}
+scalar& scalar::operator=(scalar &&toMove) noexcept
+{
+	setK(std::move(toMove.getK()));
+	setX(std::move(toMove.getX()));
+	return *this;
+}
+scalar::~scalar()
+{
 
+}
+std::complex<double>	scalar::get(long long int n)
+{
+	return getK()*x->get(n);
+}
+const std::complex<double>& scalar::getK() const
+{
+	return k;
+}
+
+void scalar::setK(const std::complex<double> &k)
+{
+	this->k = k;
+}
+
+signal* scalar::getX()
+{
+	return x;
+}
+
+void scalar::setX(signal *x)
+{
+	this->x = x;
+}
--- scalar.hpp
+++ scalar.hpp
@@ -1,6 +1,29 @@
 #ifndef DPSCPP_SCALAR_HPP_
 #define DPSCPP_SCALAR_HPP_
+#include "signal.hpp"
+#include <complex>
 
+class scalar : public signal
+{
+	public:
+		scalar(std::complex<double> k,signal *x);
+		scalar(scalar &toCopy) noexcept;
+		scalar& operator=(scalar &toCopy) noexcept;
+		scalar(scalar &&toMove) noexcept;
+		scalar& operator=(scalar &&toMove) noexcept;
+		~scalar();
+		std::complex<double>	get(long long int n);
+		const std::complex<double>& getK() const;
+		void setK(const std::complex<double> &k);
+		signal* getX();
+		void setX(signal *x);
+
+	private:
+		std::complex<double> k;
+		signal *x;
+
+
+};
 
 
 
