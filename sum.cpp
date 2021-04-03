5e1f6cdea9d6dae46a17a448a8629f78da5a9193
jjackbauer <j_r_style@hotmail.com>
2021-04-02 21:27:12 -0300

    addition of signals implemented

--- sum.cpp
+++ sum.cpp
@@ -1,1 +1,53 @@
 #include "sum.hpp"
+sum::sum(signal *x, signal *y)
+{
+	setX(x);
+	setY(y);
+}
+sum::sum(sum &toCopy) : x{toCopy.getX()}, y{toCopy.getY()}
+{
+
+}
+sum& sum::operator=(sum &toCopy)
+{
+	this->setX(toCopy.getX());
+	this->setY(toCopy.getY());
+	return *this;
+}
+sum::sum(sum &&toMove) : x{std::move(toMove.getX())}, y{std::move(toMove.getY())}
+{
+
+}
+sum& sum::operator=(sum &&toMove)
+{
+	setX(std::move(toMove.getX()));
+	setY(std::move(toMove.getY()));
+	return *this;
+}
+sum::~sum()
+{
+
+}
+std::complex<double>	sum::get(long long int n)
+{
+	return x->get(n)+y->get(n);
+}
+signal* sum::getX()
+{
+	return x;
+}
+
+void sum::setX(signal *x)
+{
+	this->x = x;
+}
+
+signal* sum::getY()
+{
+	return y;
+}
+
+void sum::setY( signal *y)
+{
+	this->y = y;
+}
--- sum.hpp
+++ sum.hpp
@@ -1,5 +1,26 @@
 #ifndef DPSCPP_SUM_HPP_
 #define DPSCPP_SUM_HPP_
+#include "signal.hpp"
+
+class sum : public signal
+{
+	public:
+		sum(signal *x, signal *y);
+		sum(sum &toCopy) noexcept;
+		sum& operator=(sum &toCopy) noexcept;
+		sum(sum &&toMove) noexcept;
+		sum& operator=(sum &&toMove) noexcept;
+		~sum();
+		std::complex<double>	get(long long int n);
+		signal* getX() ;
+		void setX(signal *x);
+		signal* getY() ;
+		void setY(signal *y);
+
+	private:
+		signal *x;
+		signal *y;
+};
 
 
 
