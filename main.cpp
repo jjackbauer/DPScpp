1a2f883429194bcf6cd94514f2dc466b68a7717b
jjackbauer <j_r_style@hotmail.com>
2021-04-08 13:11:29 -0300

    cleaning the project

--- main.cpp
+++ main.cpp
@@ -37,8 +37,6 @@ void question1A()
 	std::vector<std::complex<double>> seq;
 	seq = x.getSequence(begin, end);
 	print(seq);
-
-
 }
 void question1B()
 {
@@ -138,7 +136,6 @@ void question4A()
 
 int main ()
 {
-
 	question1A();
 	question1B();
 	question1C();
@@ -147,67 +144,4 @@ int main ()
 	question2B();
 	question3A();
 	question4A();
-
-
-
-
-
-
-	/*int begin=-10,end=10;
-	step degrau;
-	impulse impulso;
-	//std::cout<<"Antes de AQUI 0";
-	cosine	cossenoide(0.05,90.0);
-	exponential exponencial(std::complex < double > (1/12.0 , M_PI/6.0));
-	//std::cout<<"AQUI 0";
-
-	delay degrauAtrasado(4,&degrau);
-	//std::cout<<"AQUI0.1";
-	convolution H(40,&impulso,&degrauAtrasado);
-	signal *sinalDegrau=&degrau,*sinalImpulso= &impulso,*sinalCossenoide=&cossenoide,*sinalExponencial=&exponencial;
-	std::vector<std::complex<double>> sequenceSinalDegrau, sequenceSinalImpulso,sequenceSinalCossenoide, sequenceSinalExponencial,sequenceDegrauAtrasado, SequenceConvolucao;
-
-	sequenceSinalDegrau = (*sinalDegrau).getSequence(begin, end);
-	sequenceSinalImpulso = sinalImpulso->getSequence(begin, end);
-	sequenceSinalCossenoide = sinalCossenoide->getSequence(begin, end);
-	sequenceSinalExponencial = sinalExponencial->getSequence(begin, end);
-	sequenceDegrauAtrasado =   degrauAtrasado.getSequence(begin, end);
-	//std::cout<<"AQUI"<<H.get(0)<<std::endl;
-	SequenceConvolucao =  H.getSequence(-40, 40);
-	//std::cout<<"AQUI 2";
-
-
-	for(auto valor : sequenceSinalDegrau)
-			std::cout<<std::real(valor)<<" ";
-	std::cout<<std::endl;
-
-	for(auto valor : sequenceSinalImpulso)
-			std::cout<<std::real(valor)<<" ";
-	std::cout<<std::endl;
-
-	for(auto valor : sequenceSinalCossenoide)
-		std::cout<<std::real(valor)<<" ";
-	std::cout<<std::endl;
-
-	for(auto valor : sequenceSinalExponencial)
-		std::cout<<std::real(valor)<<" ";
-	std::cout<<std::endl;
-
-	for(auto valor : sequenceSinalExponencial)
-		std::cout<<std::imag(valor)<<" ";
-	std::cout<<std::endl;
-
-	for(auto valor : sequenceDegrauAtrasado)
-		std::cout<<std::real(valor)<<" ";
-	std::cout<<std::endl;
-
-	for(auto valor : SequenceConvolucao)
-		std::cout<<std::real(valor)<<" ";
-	std::cout<<std::endl;//*/
-
-
-
-
-
-
 }
